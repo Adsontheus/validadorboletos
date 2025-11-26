1886e9ed3bfa5c476fdcd1d7e7d9ba1eabcefcd7
Ádson Matheus <adsons774@gmail.com>
2025-11-26 02:09:39 -0300

    Pasta criada para arquivos para exceções

---
+++ src/main/java/com/projetos/validadorboletos/exception/BoletoInvalidoException.java
@@ -0,0 +1,6 @@
+package com.projetos.validadorboletos.exception;
+
+public class BoletoInvalidoException extends RuntimeException {
+    public BoletoInvalidoException(String msg) {
+        super(msg);
+    }
---
+++ src/main/java/com/projetos/validadorboletos/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.projetos.validadorboletos.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BoletoInvalidoException.class)
+    public ResponseEntity<?> handleBoletoInvalido(BoletoInvalidoException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of(
+                        "erro", e.getMessage(),
+                        "timestamp", LocalDateTime.now()
+                ));
+    }
+}
